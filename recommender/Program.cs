273d7341ecb26e5c4ab93b8c73fac5f6c09604de
ornwipa <fahornwipa@gmail.com>
2020-08-26 11:19:29 -0600

    completed User class

--- recommender/Program.cs
+++ recommender/Program.cs
@@ -15,8 +15,16 @@ namespace recommender
         public static void Main(string[] args)
         {
             // CreateHostBuilder(args).Build().Run();
-            // testCsvParser();
-            constructUserMatrix();
+            testCsvParser();
+
+            int[][] user_matrix = constructUserMatrix();
+
+            Console.Write("Enter user_id: "); // will be replace with UI
+            string user_row = Console.ReadLine(); // will be replace with UI
+            int user_index = Convert.ToInt32(user_row);
+
+            User current_user = accessUser(user_matrix, user_index);
+            Console.WriteLine("{0}", current_user.rating[2319]); // non-zero one
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -50,7 +58,7 @@ namespace recommender
             Console.WriteLine("Last user_id: " + ratings[n_ratings - 1].user_id);
             Console.WriteLine("Last rating: " + ratings[n_ratings - 1].rating_);
         }
-        public static void constructUserMatrix()
+        public static int[][] constructUserMatrix()
         {
             var ratings = TinyCsvParserRating.ReadRatingCsv();
             // Console.WriteLine(ratings[110896].user_id);
@@ -61,10 +69,28 @@ namespace recommender
                 data_matrix[user_row] = new int[10000]; // initialize inner elements
             }
             // Console.WriteLine(data_matrix[53423][9999]);
-            for (int m = 0; m < 981756; m++) //ratings.Count; m++)
+            for (int m = 0; m < ratings.Count; m++)
             {
                 data_matrix[ratings[m].user_id-1][ratings[m].book_id-1] = ratings[m].rating_;
             }
+            return data_matrix;
+        }
+        public static User accessUser(int[][] user_matrix, int user_index)
+        {
+            User current_user = new User();
+            current_user.rating = user_matrix[user_index];
+            int no_book_rated = CountNonZero(current_user.rating);
+            Console.WriteLine("The user rated {0} books.", no_book_rated);
+            return current_user;
+        }        
+        public static int CountNonZero<T>(T[] arg) // to be used for any vectors
+        {
+            int no_elements = 0;
+            for (int k = 0; k < arg.Length; k++) 
+            {                
+                if (Convert.ToInt32(arg[k]) != 0) no_elements++; 
+            }
+            return no_elements;
         }
     }
 }
