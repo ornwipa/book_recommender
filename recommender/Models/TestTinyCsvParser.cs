9e78e9c452d5ae40cdcbb43bb026de343b42ea14
ornwipa <fahornwipa@gmail.com>
2020-08-27 09:38:45 -0600

    see previous commit

---
+++ recommender/Models/TestTinyCsvParser.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace recommender.Models
+{
+    public class Test
+    {
+        public static void testCsvParser()
+        {
+            var records = TinyCsvParserBook.ReadBookCsv();
+            int n_books = records.Count;
+            Console.WriteLine("Number of books " + n_books);
+            Console.WriteLine("Type of records " + records[0]);
+            Console.WriteLine("id " + records[0].id);
+            Console.WriteLine("book_id " + records[0].book_id);
+            Console.WriteLine("isbn " + records[0].isbn);
+            Console.WriteLine("authors " + records[0].authors);
+            Console.WriteLine("year " + records[0].year);
+            Console.WriteLine("title " + records[0].title);
+            Console.WriteLine("image_url " + records[0].image_url);
+
+            var ratings = TinyCsvParserRating.ReadRatingCsv();
+            int n_ratings = ratings.Count;
+            Console.WriteLine("Number of ratings " + n_ratings);
+            Console.WriteLine("First book_id: " + ratings[0].book_id);
+            Console.WriteLine("First user_id: " + ratings[0].user_id);
+            Console.WriteLine("First rating: " + ratings[0].rating_);
+            Console.WriteLine("Last book_id: " + ratings[n_ratings - 1].book_id);
+            Console.WriteLine("Last user_id: " + ratings[n_ratings - 1].user_id);
+            Console.WriteLine("Last rating: " + ratings[n_ratings - 1].rating_);
+        }
+    }
+}
--- recommender/Models/User.cs
+++ recommender/Models/User.cs
@@ -5,5 +5,31 @@ namespace recommender.Models
     public class User
     {
         public int[] rating { set; get; }
-    }
+
+        public static int[][] constructUserMatrix()
+        {
+            var ratings = TinyCsvParserRating.ReadRatingCsv();
+
+            int[][] data_matrix = new int[53424][]; // n_users is to be replaced later            
+            for (int user_row = 0; user_row < 53424; user_row++)
+            {
+                data_matrix[user_row] = new int[10000]; // initialize inner elements to 0
+            }
+            
+            for (int m = 0; m < ratings.Count; m++)
+            {
+                data_matrix[ratings[m].user_id-1][ratings[m].book_id-1] = ratings[m].rating_;
+            }
+            return data_matrix;
+        }
+
+        public static User accessUser(int[][] user_matrix, int user_index)
+        {
+            User current_user = new User();
+            current_user.rating = user_matrix[user_index];
+            int no_book_rated = VectorOpt.CountNonZero(current_user.rating);
+            Console.WriteLine("The user rated {0} books.", no_book_rated);
+            return current_user;
+        }  
+    } 
 }
